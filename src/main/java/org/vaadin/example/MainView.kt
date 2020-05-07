43d667698c243d7478e95d4378adc6f51b7583a7
Martin Vysny <mavi@vaadin.com>
2020-05-07 10:53:30 +0300

    Added Kotlin files

---
+++ src/main/java/org/vaadin/example/MainView.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.vaadin.example
+
+import com.github.mvysny.karibudsl.v10.*
+import com.vaadin.flow.component.Key
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.dependency.CssImport
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.router.Route
+import com.vaadin.flow.server.PWA
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+@PWA(name = "Project Base for Vaadin", shortName = "Project Base")
+@CssImport.Container(value = [  // repeatable annotations are not supported by Kotlin, please vote for https://youtrack.jetbrains.com/issue/KT-12794
+    CssImport("./styles/shared-styles.css"),
+    CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+])
+class MainView : KComposite() {
+    private lateinit var nameField: TextField
+    private lateinit var greetButton: Button
+
+    // The main view UI definition
+    private val root = ui {
+        verticalLayout {
+            // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
+            addClassName("centered-content")
+
+            // Use TextField for standard text input
+            nameField = textField("Your name")
+
+            // Use Button for a clickable button
+            greetButton = button("Say hello") {
+                setPrimary(); addClickShortcut(Key.ENTER)
+            }
+        }
+    }
+
+    init {
+        // attach functionality to the UI components.
+        // It's a good practice to keep UI functionality separated from UI definition.
+
+        // Button click listeners can be defined as lambda expressions
+        greetButton.onLeftClick {
+            Notification.show("Hello, ${nameField.value}")
+        }
+    }
+}
---
+++ src/test/java/org/vaadin/example/MainViewTest.kt
@@ -0,0 +1,73 @@
+package org.vaadin.example
+
+import com.github.mvysny.kaributesting.v10.*
+import com.vaadin.flow.component.UI
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.textfield.TextField
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+/**
+ * Tests the UI. Uses the Browserless testing approach as provided by the
+ * [Karibu Testing](https://github.com/mvysny/karibu-testing) library.
+ */
+class MainViewTest {
+
+    companion object {
+        private lateinit var routes: Routes
+
+        @BeforeAll
+        @JvmStatic
+        fun computeRoutes() {
+            // Route discovery involves classpath scanning and is an expensive operation.
+            // Running the discovery process only once per test run speeds up the test runtime considerably.
+            // Discover the routes once and cache the result.
+            routes = Routes().autoDiscoverViews("org.vaadin.example")
+        }
+    }
+
+    @BeforeEach
+    fun mockVaadin() {
+        // MockVaadin.setup() registers all @Routes, prepares the Vaadin instances for us
+        // (the UI, the VaadinSession, VaadinRequest, VaadinResponse, ...) and navigates to the root route.
+        MockVaadin.setup(routes)
+    }
+
+    @AfterEach
+    fun tearDownVaadin() {
+        MockVaadin.tearDown()
+    }
+
+    @Test
+    fun `smoke test`() {
+        // Smoke test is a quick test to check that the basic machinery is in place and works.
+        // The analogy would be to turn on an electric device (e.g. a coffee maker)
+        // then turn it off immediately without even checking whether it actually works or not,
+        // and watch whether there is any smoke. If yes, the coffee maker is
+        // probably burning from a short-circuit and any further tests are pointless.
+
+        // The root route should be set directly in the UI; let's check whether it is so.
+        // This demoes the direct access to the UI and its children and grand-children,
+        // which encompasses all visible Vaadin components.
+        val main: MainView = UI.getCurrent().children.findFirst().get() as MainView
+
+        // However when using this kind of low-level lookups, the code quickly gets
+        // pretty complicated. Let's use the _get() function instead,
+        // which will walk the UI tree for us.
+        _expectOne<MainView>()
+    }
+
+    @Test
+    fun `test greeting`() {
+        // simulate an user input
+        _get<TextField> { caption = "Your name" } ._value = "Martin"
+
+        // simulate a button click as if clicked by the user
+        _get<Button> { caption = "Say hello" } ._click()
+
+        // look up the notification and assert on its value
+        expectNotifications("Hello, Martin")
+    }
+}
